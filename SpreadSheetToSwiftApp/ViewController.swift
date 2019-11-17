985f9ae739ecef19ea0f545abeb458efcefae554
Hiroki Kondo <kondo.matr02@gmail.com>
2019-11-17 23:30:18 +0900

    同期処理、非同期処理を追加

--- SpreadSheetToSwiftApp/ViewController.swift
+++ SpreadSheetToSwiftApp/ViewController.swift
@@ -27,34 +27,49 @@ class ViewController: UIViewController {
     // MARK: - API通信
     func getData(){
         
-        let url = "https://script.googleusercontent.com/macros/echo?user_content_key=oQatZCIiRtyN8zRbYNETqdtWzaVdxQ8Q4GzEguOkme0mWqTpzpZENZ5dov-5xYf_jvcyKKES-2525T6XGrI7BgYwpFZjZZQUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLB_e7VVX3kcOGALXEI7BCPwlVjsy1Wpw0pLEpxgFh9MehdypQ3WWIVFghBl97br2w&lib=MYmh94ByISnwLCOaItycI2hSBcQ8BraCT"
-        
-        //Alamofireを使ってhttpsリクエスト
-        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+        //非同期処理開始
+        DispatchQueue.global().async {
             
-            //responceをswith文で分岐する
-            switch response.result{
-            case .success:
-                //swiftyJSONを使って、JSON解析
+            //同期処理開始
+            DispatchQueue.main.async {
                 
-                for i in 0...19{
-                    let json:JSON = JSON(response .data as Any)
-                    let ID_String = json["jsondata"][i]["id"].string
-                    let Name_String = json["jsondata"][i]["name"].string
-                    
-                    self.ID_Array.append(ID_String!)
-                    self.Name_Array.append(Name_String!)
-                    
-                    print("hirohiro1")
-                    print(self.ID_Array)
-                    print("hirohiro2")
-                    print(self.Name_Array)
+                let url = "https://script.googleusercontent.com/macros/echo?user_content_key=oQatZCIiRtyN8zRbYNETqdtWzaVdxQ8Q4GzEguOkme0mWqTpzpZENZ5dov-5xYf_jvcyKKES-2525T6XGrI7BgYwpFZjZZQUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLB_e7VVX3kcOGALXEI7BCPwlVjsy1Wpw0pLEpxgFh9MehdypQ3WWIVFghBl97br2w&lib=MYmh94ByISnwLCOaItycI2hSBcQ8BraCT"
+                       
+                //Alamofireを使ってhttpsリクエスト
+                Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+                           
+                    //responceをswith文で分岐する
+                    switch response.result{
+                    case .success:
+                        
+                        //swiftyJSONを使って、JSON解析
+                        for i in 0...19{
+                            let json:JSON = JSON(response .data as Any)
+                            let ID_String = json["jsondata"][i]["id"].string
+                            let Name_String = json["jsondata"][i]["name"].string
+                            
+                            self.ID_Array.append(ID_String!)
+                            self.Name_Array.append(Name_String!)
+                                   
+                            print("hirohiro1")
+                            print(self.ID_Array)
+                            print("hirohiro2")
+                            print(self.Name_Array)
+                            
+                        }
+                        
+                    case .failure(let error):
+                        print(error)
+            
+                    }
                     
                 }
-            case .failure(let error):
-                print(error)
+            
             }
+            //同期処理終了
+        
         }
+        //非同期処理終了
         
     }
     
